1f45e4b5d3b6e35f8689dea4a1fdc9e7d7b68177
Aidan <aidanchoi@arpainfinity.com>
2025-05-20 23:06:02 +0800

    test: route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="search" options={{ title: "Search" }} />
+      {/* <Stack.Screen name="search" options={{ title: "Search" }} /> */}
     </Stack>
   );
 }
