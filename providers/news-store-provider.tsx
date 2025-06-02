e388bb4de80543f503518a81025088cabd0da4d0
Aidan <21993381+rockie2695@users.noreply.github.com>
2025-06-03 01:08:13 +0800

    test

---
+++ providers/news-store-provider.tsx
@@ -0,0 +1,40 @@
+// src/providers/counter-store-provider.tsx
+"use client";
+
+import { type ReactNode, createContext, useRef, useContext } from "react";
+import { useStore } from "zustand";
+
+import { type NewsStore, createNewsStore } from "@/stores/news-store";
+
+export type NewsStoreApi = ReturnType<typeof createNewsStore>;
+
+export const NewsStoreContext = createContext<NewsStoreApi | undefined>(
+  undefined
+);
+
+export interface NewsStoreProviderProps {
+  children: ReactNode;
+}
+
+export const NewsStoreProvider = ({ children }: NewsStoreProviderProps) => {
+  const storeRef = useRef<NewsStoreApi | null>(null);
+  if (storeRef.current === null) {
+    storeRef.current = createNewsStore();
+  }
+
+  return (
+    <NewsStoreContext.Provider value={storeRef.current}>
+      {children}
+    </NewsStoreContext.Provider>
+  );
+};
+
+export const useNewsStore = <T,>(selector: (store: NewsStore) => T): T => {
+  const newsStoreContext = useContext(NewsStoreContext);
+
+  if (!newsStoreContext) {
+    throw new Error(`useNewsStore must be used within NewsStoreProvider`);
+  }
+
+  return useStore(newsStoreContext, selector);
+};
