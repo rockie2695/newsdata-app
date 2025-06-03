ce52c595e7b76c6264a72341bd8199a008ffd9bc
Aidan <21993381+rockie2695@users.noreply.github.com>
2025-06-04 01:36:03 +0800

    test

--- app/(tabs)/news/index.tsx
+++ app/(tabs)/news/index.tsx
@@ -2,6 +2,7 @@ import { Text, ScrollView } from "react-native";
 import TopTabFlatList from "@/components/Category/TopTabFlatList";
 import MainSlide from "@/components/Category/MainSlide";
 import { useNewsStore } from "@/providers/news-store-provider";
+import CategoryFlatList from "@/components/Category/CategoryFlatList";
 export default function Index() {
   const category = useNewsStore((state) => state.category);
   return (
@@ -9,6 +10,14 @@ export default function Index() {
       <TopTabFlatList />
       <Text>Category screen {category}</Text>
       <MainSlide />
+      {category === "home" &&
+        [
+          "top",
+          "business",
+          "entertainment",
+          "sports",
+          "technology",
+        ].map((item) => <CategoryFlatList key={item} category={item} />)}
     </ScrollView>
   );
 }
---
+++ components/Category/CategoryFlatList.tsx
@@ -0,0 +1,5 @@
+import { View } from "react-native";
+
+export default function CategoryFlatList({ category }: { category: string }) {
+  return <View></View>;
+}
